0726ed5508f4924856f56f334c2f9d1ba1088c69
vodoanhoanglong <67463870+vodoanhoanglong@users.noreply.github.com>
2020-11-13 09:41:38 +0700

    Update Nam_nhuan.asm

--- Nam_nhuan.asm
+++ Nam_nhuan.asm
@@ -1,85 +1,58 @@
-.model small 
-.stack 100h
-.data
+org 100h  
 
-leap dw 'Leap year$'
-nleap dw 'Not Leap year$'
-a dw ?
-b dw ?
-c dw ?
-.code
-string macro str
-    
-    push ax
-    push dx
-    
-    mov dx,offset str
-    mov ah,9         
-    int 21h
-    
-    pop dx
-    pop ax
-endm
+include 'emu8086.inc'    
 
-main proc
-    mov ax,@data
-    mov ds,ax
-    
-    call indec; ax value
-    push ax
-    
-    mov bx,400
-    div bx  ; ax=ax/400
-    mov a,dx
-    mov dx,0
-    
-    pop ax
-    push ax
-    
-    mov bx,4
-    div bx  ;ax=ax/4
-    mov b,dx
-    mov dx,0
-    
-    pop ax
-    
-    push ax
-    
-    mov bx,100
-    div bx; ax=ax/100
-    mov c,dx
-    mov dx,0
-    
-    pop ax
-    call endl
-    
-    cmp a,0
-    jne @else1
-    
-    string leap
-    jmp @endelse
-    
-    @else1:
-    
-    cmp b,0
-    jne @else2
-    cmp c,0
-    je  @else2
-    
-    string leap
-    
-    jmp @endelse
-    
-    @else2:
-    
-    string nleap
-    
-    @endelse:
-    
-    
+mov cx, 04h
 
-    mov ax,4ch
-    int 21h
-    main endp
-include io.lib
-end main
+ 
+inp:    mov ah, 01h
+        int 21h          
+
+        sub al, 30h    
+        mov bl, al
+        mov ax, dx
+        mov dx, 10h
+        mul dx     
+        
+        mov bh, 00h
+        add ax, bx
+        mov dx, ax
+    loop inp      
+          
+            
+mov ax, dx
+mov dx, 0000h
+mov bx, 64h
+mul bx
+mov bx, 100h
+div bx 
+mov cx, ax
+mov dx, 0000h
+            
+mov bx, 400h
+div bx
+jz yes              
+
+mov ax, cx
+mov bx, 04h
+mov dx, 0000h
+div bx
+jnz no
+
+mov ax, cx   
+mov dx, 0000h
+mov bx, 100h
+div bx
+jnz no
+jz yes
+
+no: 
+    print 'Not a  Leap Year'
+    jmp exit
+
+yes:
+    print 'Leap Year'
+
+exit:
+
+ret
